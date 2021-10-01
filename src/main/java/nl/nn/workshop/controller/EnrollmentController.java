b1c7dd53b6abcd522bba0d5807ebbd93be98b101
Cristiano Santos <cristiano.monteiro.dos.santos@nn.nl>
2021-10-01 13:07:05 +0200

    moving common path to controller level request mapping

--- src/main/java/nl/nn/workshop/controller/EnrollmentController.java
+++ src/main/java/nl/nn/workshop/controller/EnrollmentController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/enrollments")
 public class EnrollmentController {
 
   private final EnrollmentService enrollmentService;
@@ -18,26 +20,26 @@ public class EnrollmentController {
     this.enrollmentService = enrollmentService;
   }
 
-  @PostMapping(value = "/enrollments/student/{studentId}/course/{courseId}")
+  @PostMapping(value = "/student/{studentId}/course/{courseId}")
   public ResponseEntity<EnrollmentResource> createEnrollment(
       @PathVariable(value = "studentId") Long studentId,
       @PathVariable(value = "courseId") Long courseId) {
     return ResponseEntity.ok(enrollmentService.create(studentId, courseId));
   }
 
-  @GetMapping(value = "/enrollments/student/{studentId}/course/{courseId}")
+  @GetMapping(value = "/student/{studentId}/course/{courseId}")
   public ResponseEntity<EnrollmentResource> getEnrollmentById(
       @PathVariable(value = "studentId") Long studentId,
       @PathVariable(value = "courseId") Long courseId) {
     return ResponseEntity.ok(enrollmentService.findById(studentId, courseId));
   }
 
-  @GetMapping(value = "/enrollments")
+  @GetMapping
   public ResponseEntity<Iterable<EnrollmentResource>> getAllEnrollments() {
     return ResponseEntity.ok(enrollmentService.findAll());
   }
 
-  @DeleteMapping(value = "/enrollments/student/{studentId}/course/{courseId}")
+  @DeleteMapping(value = "/student/{studentId}/course/{courseId}")
   public ResponseEntity<Void> deleteEnrollmentById(
       @PathVariable(value = "studentId") Long studentId,
       @PathVariable(value = "courseId") Long courseId) {
